28851cfb693f1829b9dd426316fa6e20ca169e14
Matt <mbrowne108@gmail.com>
2022-04-13 12:30:16 -0400

    Finished

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,28 @@
+// function fibonacci(num) {
+//   let n1 = 0, n2 = 1, temp
+
+//   while (num >= 0) {
+//     temp = n1
+//     n1 = n1 + n2
+//     n2 = temp
+//     num--
+//   }
+//   return n2
+// }
+
 function fibonacci(num) {
-  // type your code here
+  let i
+  let fib = []
+  fib[0] = 0
+  fib[1] = 1
+  for (i = 2; i < 50; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1]
+  }
+  return fib[num]
 }
+
+console.log(fibonacci(28))
+
 
 if (require.main === module) {
   // add your own tests in here
